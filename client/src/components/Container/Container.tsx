8f77769cd4ea16daebca42af9aaa21f3f9b64daa
AndriyPapirnyk <papirnykandriy@gmail.com>
2024-01-16 17:40:26 +0200

    fix

--- client/src/components/Container/Container.tsx
+++ client/src/components/Container/Container.tsx
@@ -2,7 +2,7 @@ import './Container.scss'
 const Container = () => {
     return (
         <div>
-            <input type="text" className='container__input' />
+            <input placeholder='qweqweqwe' type="text" className='container__input' />
             <button className='container__button'>Container</button>
         </div>
     )
