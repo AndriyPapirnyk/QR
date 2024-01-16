01c080ac49c267749006eafe6d73c77a8ba950bd
UserName <m492615892gmail.com>
2024-01-16 18:49:15 +0200

    add styles to Container

--- client/src/components/Container/Container.tsx
+++ client/src/components/Container/Container.tsx
@@ -1,9 +1,31 @@
-import './Container.scss'
+import './Container.scss';
+
+import image from './Image/QRCode.png'
 const Container = () => {
     return (
-        <div>
-            <input placeholder='qweqweqwe' type="text" className='container__input' />
-            <button className='container__button'>Container</button>
+        <div className='container'>
+            <div className='container__first-block'>
+                <div className="container__first-block_image">
+                    <img src={image} alt="" />
+                </div>
+
+            </div>
+            <div className='container__second-block'>
+                <h3>Відскановано</h3>
+                <div className='container__second-block_card-container'>
+                    <div className="container__second-block_card">
+                        <p>4</p>
+                    </div>
+                    <div className="container__second-block_card">
+                        <p>2</p>
+                    </div>
+                    <div className="container__second-block_card">
+                        <p>8</p>
+                    </div>
+                </div>
+            </div>
+
+
         </div>
     )
 }
